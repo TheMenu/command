931e66f21913a69726b5b2b9934fd150847f7cc8
Jérémie <jeremie.bonal@gmail.com>
2021-02-18 16:16:55 +0100

    Fix I18n load path modification.

--- lib/command/i18n.rb
+++ lib/command/i18n.rb
@@ -1,4 +1,3 @@
 require 'i18n'
 
-I18n.load_path << Dir[File.expand_path("locales") + "/*.yml"]
-I18n.default_locale = :en # (note that `en` is already the default!)
+I18n.load_path += Dir[File.expand_path("locales") + "/*.yml"]
